5f940831b5c42ff232091c801781db06e2b54db5
reshmamanunair <reshmanair2014@gmail.com>
2022-04-01 20:37:55 -0600

    i am commiting the selenium projects

--- seleniumprojects/src/seleniumprojects/FirstScript.java
+++ seleniumprojects/src/seleniumprojects/FirstScript.java
@@ -8,14 +8,25 @@ public class FirstScript {
 		// TODO Auto-generated method stub
 		//path of the driver
 		System.setProperty("webdriver.chrome.driver", "E:\\selenium\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.gecko.driver", "E:\\selenium\\chromedriver_win32\\chromedriver.exe");
 //invoking the browser
 		ChromeDriver driver=new ChromeDriver();
+		//ChromeDriver driver=new FirefoxDriver();
+		//WebDriver Driver=new ChromeDriver();
 //accessing the url
 driver.get("http://www.google.co.in/");
 //or
-driver.navigate().to("http://www.google.co.in/");
+//driver.navigate().to("http://www.google.co.in/");
+//driver.getTitle();
+System.out.println(driver.getTitle());
+//driver.getCurrentUrl();
+System.out.println(driver.getCurrentUrl());
 //closing the browser
 driver.close();
+//driver.quit();
+driver.resetInputState();//firefox and edge class has no knowledge of this
+
+
 
 	}
 
